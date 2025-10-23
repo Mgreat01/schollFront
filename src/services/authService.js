45fa588a6311721c7cc9ae52b56304054d426aae
Mgreat01 <ephraimmonga5@gmail.com>
2025-10-23 15:52:05 +0200

    implementation de src/services/authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,1 +1,17 @@
-// authService.js - service de l'application
+import api, { setAuthToken } from "./api";
+
+const login = async ({ email, password }) => {
+  const res = await api.post("/auth/login", { email, password });
+  const data = res.data;
+  if (data.token) setAuthToken(data.token);
+  return data;
+};
+
+const registerSchool = async (payload) => {
+  const res = await api.post("/auth/register-school", payload);
+  return res.data;
+};
+
+const setToken = (token) => setAuthToken(token);
+
+export default { login, registerSchool, setToken };
