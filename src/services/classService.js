a66665920b0856253c4e8a9df48a4fb671f0cca1
Mgreat01 <ephraimmonga5@gmail.com>
2025-10-23 15:53:32 +0200

    implementation de src/services/classService.js

--- src/services/classService.js
+++ src/services/classService.js
@@ -1,1 +1,3 @@
-// classService.js - service de l'application
+import api from "./api";
+export const fetchClasses = async () => (await api.get("/classes")).data;
+export const createClass = async (payload) => (await api.post("/classes", payload)).data;
