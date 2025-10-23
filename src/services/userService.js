cd4724ec838ad9ee080e98d592596e9c2324ce02
Mgreat01 <ephraimmonga5@gmail.com>
2025-10-23 15:52:49 +0200

    impl de src/services/userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,1 +1,16 @@
-// userService.js - service de l'application
+import api from "./api";
+
+export const fetchUsers = async () => {
+  const res = await api.get("/users");
+  return res.data;
+};
+
+export const createUser = async (payload) => {
+  const res = await api.post("/users", payload);
+  return res.data;
+};
+
+export const authorizeUser = async (id, canManageUsers) => {
+  const res = await api.put(`/users/${id}/authorize`, { canManageUsers });
+  return res.data;
+};
