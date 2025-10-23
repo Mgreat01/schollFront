d197f8c59423b2989cfaea5f1aae59b28bc384d5
Mgreat01 <ephraimmonga5@gmail.com>
2025-10-23 15:54:06 +0200

    implementation de src/services/studentService.js

--- src/services/studentService.js
+++ src/services/studentService.js
@@ -1,1 +1,3 @@
-// studentService.js - service de l'application
+import api from "./api";
+export const fetchStudents = async () => (await api.get("/students")).data;
+export const createStudent = async (payload) => (await api.post("/students", payload)).data;
